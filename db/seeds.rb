4d82a8cb8bd9302a005aa6c706f6574c68d4f672
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-21 15:05:27 -0600

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pet.destroy_all
+Shelter.destroy_all
+Veterinarian.destroy_all
+VeterinaryOffice.destroy_all
+
+vet_office_1 = VeterinaryOffice.create(name: 'Best Vets', boarding_services: true, max_patient_capacity: 27)
+vet_office_2 = VeterinaryOffice.create(name: 'Vets R Us', boarding_services: true, max_patient_capacity: 20)
+not_on_call_vet = vet_office_1.veterinarians.create(name: 'Sam', review_rating: 10, on_call: false)
+vet_1 = vet_office_1.veterinarians.create(name: 'Taylor', review_rating: 10, on_call: true)
+vet_2 = vet_office_1.veterinarians.create(name: 'Jim', review_rating: 8, on_call: true)
+vet_3 = vet_office_2.veterinarians.create(name: 'Sarah', review_rating: 9, on_call: true)
+vet_4 = vet_office_2.veterinarians.create(name: 'Janet', review_rating: 6, on_call: true)
+
+
+shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+
+pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+pet_3 = shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+pet_4 = shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
