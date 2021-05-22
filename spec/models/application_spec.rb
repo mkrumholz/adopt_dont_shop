653deffb0df8c3be0824c462f8b5ce0cf7c10b76
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-21 19:56:33 -0600

    Complete test coverage for application model

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Application do
     it {should have_many :pet_applications}
     it {should have_many(:pets).through(:pet_applications)}
   end
+
+  describe 'instance methods' do
+    describe '#display_status' do
+      it 'displays the status as a string' do
+        @frizz = Application.create!(name: 'Ms. Frizzle', address: '1 Magic Schoolbus Rd, Walkerville, MD, 01010', description: 'Because I am a boss.', status: :in_progress)
+
+        expect(@frizz.display_status).to eq 'In Progress'
+      end
+    end
+  end
 end
