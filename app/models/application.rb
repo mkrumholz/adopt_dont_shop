025cb39bd0fc2050c6bcba3616e3f6c8354450e8
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-23 16:32:19 -0600

    Add validations to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates_format_of :zip_code, :with => /\A[0-9]{5}(?:-[0-9]{4})?\z/, :message => 'does not have a valid format'
+  validates :description, presence: true
   has_many :pet_applications, dependent: :destroy
   has_many :pets, through: :pet_applications
 
