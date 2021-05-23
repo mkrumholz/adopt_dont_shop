9f8dbfed418af658c2a39c9a30b0bfc4e0a0e3e1
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-23 16:33:09 -0600

    Create display method for application address

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -9,4 +9,22 @@ class ApplicationsController < ApplicationController
 
   def new
   end
+
+  def create
+    application = Application.create(application_params)
+
+    if application.save
+      redirect_to "/applications/#{application.id}"
+    else
+      redirect_to "/applications/new"
+      flash[:alert] = "Error: #{error_message(application.errors)}"
+    end
+
+  end
+
+  private
+    def application_params
+      params.permit(:name, :street_address, :city, :state, :zip_code, :description)
+            .merge(status: :in_progress)
+    end
 end
--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -30,11 +30,11 @@ RSpec.describe 'new application view' do
         fill_in 'Street address', with: '1 Magic Schoolbus Rd'
         fill_in 'City', with: 'Walkerville'
         select 'Maryland', from: :state
-        fill_in 'Zip code', with: 01010
+        fill_in 'Zip code', with: '01010'
         fill_in :description, with: 'Because I am a boss.'
         click_button 'Save'
 
-        # expect(current_path).to eq "/applications/#{\/d+\}"
+        expect(current_path).to eq "/applications/#{Application.last.id}"
         expect(page).to have_content 'Ms. Frizzle'
         expect(page).to have_content '1 Magic Schoolbus Rd, Walkerville, MD, 01010'
         expect(page).to have_content 'Because I am a boss.'
@@ -49,27 +49,8 @@ RSpec.describe 'new application view' do
         click_button 'Save'
 
         expect(page).to have_current_path '/applications/new'
-        expect(page).to have_content("Error: Name can\'t be blank, Street address can\'t be blank, City can\'t be blank, State can\'t be blank, Zip code is not valid, Description can\'t be blank")
+        expect(page).to have_content("Error: Name can\'t be blank, Street address can\'t be blank, City can\'t be blank, Zip code can\'t be blank, Zip code does not have a valid format, Description can\'t be blank")
       end
     end
   end
 end
-
-#
-# As a visitor
-# When I visit the pet index page
-# Then I see a link to "Start an Application"
-# When I click this link
-# Then I am taken to the new application page where I see a form
-# When I fill in this form with my:
-#
-# Name
-# Street Address
-# City
-# State
-# Zip Code
-# And I click submit
-# Then I am taken to the new application\'s show page
-# And I see my Name, address information, and
-# description of why I would make a good home
-# And I see an indicator that this application is "In Progress"
