e23898eb1f05b5cdc5aa6e7b3e25d4116eac2331
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-23 16:31:39 -0600

    Write tests for validations on applications

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Application do
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:street_address)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zip_code)}
+    it {should allow_values('05408', '01010', '19493-8392').for(:zip_code)}
+    it {should_not allow_values('14834084', '2384', 'agdke', '24821-33').for(:zip_code)}
+    it {should validate_presence_of(:description)}
+  end
+
   describe 'relationships' do
     it {should have_many(:pet_applications).dependent(:destroy)}
     it {should have_many(:pets).through(:pet_applications)}
