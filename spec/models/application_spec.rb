dafc3282e91a922caa1e3ad6c3c221cb27817e35
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-23 11:48:46 -0600

    Ensure test coverage for models

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Application do
   describe 'relationships' do
-    it {should have_many :pet_applications}
+    it {should have_many(:pet_applications).dependent(:destroy)}
     it {should have_many(:pets).through(:pet_applications)}
   end
 
