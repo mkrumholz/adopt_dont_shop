0bbe5905510c094424d6482a7c351133abb4f168
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-24 19:07:44 -0600

    Create prelim view logic for app status

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -8,49 +8,66 @@ RSpec.describe 'the application show page' do
     @cat = @frizz.pets.create!(name: 'Catward', breed: 'bengal', age: 4, adoptable: false, shelter: @shelter)
   end
 
-  it 'shows the name of the applicant' do
-    visit "/applications/#{@frizz.id}"
+  describe 'application details' do
+    it 'shows the name of the applicant' do
+      visit "/applications/#{@frizz.id}"
 
-    expect(page).to have_content 'Ms. Frizzle'
+      expect(page).to have_content 'Ms. Frizzle'
+    end
+
+    it 'shows the full address of the applicant' do
+      visit "/applications/#{@frizz.id}"
+
+      expect(page).to have_content '1 Magic Schoolbus Rd, Walkerville, MD, 01010'
+    end
+
+    it 'shows the description given by applicant' do
+      visit "/applications/#{@frizz.id}"
+
+      expect(page).to have_content 'Because I am a boss.'
+    end
+
+    it 'shows the status of the application' do
+      visit "/applications/#{@frizz.id}"
+
+      expect(page).to have_content 'Application status: In Progress'
+    end
   end
 
-  it 'shows the full address of the applicant' do
-    visit "/applications/#{@frizz.id}"
+  describe 'pets applied for' do
+    it 'shows all names of the pets this application is for' do
+      visit "/applications/#{@frizz.id}"
 
-    expect(page).to have_content '1 Magic Schoolbus Rd, Walkerville, MD, 01010'
+      expect(page).to have_content 'Liz Ard'
+      expect(page).to have_content 'Catward'
+    end
+
+    it 'shows each pet name as a link to their show page' do
+      visit "/applications/#{@frizz.id}"
+
+      click_link 'Liz Ard'
+
+      expect(current_path).to eq "/pets/#{@liz.id}"
+    end
+
+    it 'lets user know if application has no pets yet' do
+      new_app = Application.create!(name: 'Newton', street_address: '1 New Rd', city: 'Newton', state: 'MA', zip_code: '11223', description: 'Unclear', status: :in_progress)
+
+      visit "/applications/#{new_app.id}"
+
+      within "div#applied-for" do
+        expect(page).to have_content 'None yet! To submit your application, please add at least one pet.'
+      end
+    end
   end
 
-  it 'shows the description given by applicant' do
-    visit "/applications/#{@frizz.id}"
+  describe 'adding a pet' do
+    before :each do
+      @new_pet_1 = Pet.create!(name: 'Gus', breed: 'Black Lab', age: 4, adoptable: true, shelter: @shelter)
+      @new_pet_2 = Pet.create!(name: 'Bambi', breed: 'Basic deer', age: 1, adoptable: true, shelter: @shelter)
+    end
 
-    expect(page).to have_content 'Because I am a boss.'
-  end
-
-  it 'shows the status of the application' do
-    visit "/applications/#{@frizz.id}"
-
-    expect(page).to have_content 'Application status: In Progress'
-  end
-
-  it 'shows all names of the pets this application is for' do
-    visit "/applications/#{@frizz.id}"
-
-    expect(page).to have_content 'Liz Ard'
-    expect(page).to have_content 'Catward'
-  end
-
-  it 'shows each pet name as a link to their show page' do
-    visit "/applications/#{@frizz.id}"
-
-    click_link 'Liz Ard'
-
-    expect(current_path).to eq "/pets/#{@liz.id}"
-  end
-
-  describe 'in-progress application' do
     it 'has a form to add a pet to the application' do
-      new_pet = Pet.create!(name: 'Gus', breed: 'Black Lab', age: 4, adoptable: true, shelter: @shelter)
-
       visit "/applications/#{@frizz.id}"
 
       expect(page).to have_content 'Add a Pet to this Application'
@@ -66,31 +83,25 @@ RSpec.describe 'the application show page' do
     end
 
     it 'links to the show page for each pet' do
-      new_pet_1 = Pet.create!(name: 'Gus', breed: 'Black Lab', age: 4, adoptable: true, shelter: @shelter)
-      new_pet_2 = Pet.create!(name: 'Bambi', breed: 'Basic deer', age: 1, adoptable: true, shelter: @shelter)
-
       visit "/applications/#{@frizz.id}"
 
       fill_in :search, with: 'Gus'
       click_on 'Search'
 
-      within "li#pet-#{new_pet_1.id}" do
+      within "li#pet-#{@new_pet_1.id}" do
         click_link 'More info'
       end
 
-      expect(current_path).to eq "/pets/#{new_pet_1.id}"
+      expect(current_path).to eq "/pets/#{@new_pet_1.id}"
     end
 
     it 'can add a pet to the application' do
-      new_pet_1 = Pet.create!(name: 'Gus', breed: 'Black Lab', age: 4, adoptable: true, shelter: @shelter)
-      new_pet_2 = Pet.create!(name: 'Bambi', breed: 'Basic deer', age: 1, adoptable: true, shelter: @shelter)
-
       visit "/applications/#{@frizz.id}"
 
       fill_in :search, with: 'Gus'
       click_on 'Search'
 
-      within "li#pet-#{new_pet_1.id}" do
+      within "li#pet-#{@new_pet_1.id}" do
         click_button 'Adopt this pet'
       end
 
@@ -99,6 +110,15 @@ RSpec.describe 'the application show page' do
       within "div#applied-for" do
         expect(page).to have_content 'Gus'
       end
+    end
+
+    it 'does not allow you to add a pet once application is submitted' do
+      new_app = Application.create!(name: 'Newton', street_address: '1 New Rd', city: 'Newton', state: 'MA', zip_code: '11223', description: 'Unclear', status: :pending)
+
+      visit "/applications/#{new_app.id}"
+
+      expect(page).to_not have_content 'Add a Pet to this Application'
+      expect(page).to_not have_button 'Search'
     end
   end
 end
