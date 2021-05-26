2269f90f6110db67452235a772663238e44c69da
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-26 10:28:42 -0600

    Complete basic functionality for updating app status to approved/rejected

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -6,6 +6,11 @@ module Admin
         pet_application = PetApplication.locate(params[:pet_id], @application.id)
         pet_application.update_status(params[:status])
       end
+      if @application.pet_applications.all? { |application| application.status == 'approved' }
+        @application.update!(status: :approved)
+      elsif @application.pet_applications.any? { |application| application.status == 'rejected' }      
+        @application.update!(status: :rejected)
+      end
     end
   end
 end
