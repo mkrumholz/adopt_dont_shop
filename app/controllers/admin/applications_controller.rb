9fa5b6ee5eb3fe543743602de6bf40052f3eb9c9
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-26 08:16:27 -0600

    Refactor AdminAppController to use petapp methods

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -3,8 +3,7 @@ module Admin
     def show
       @application = Application.find(params[:id])
       if params[:status].present?
-        # should refactor below line into PetApp model with `locate(id, id)`
-        pet_application = PetApplication.where(pet_id: params[:pet_id]).where(application_id: @application.id)[0]
+        pet_application = PetApplication.locate(params[:pet_id], @application.id)
         pet_application.update_status(params[:status])
         render :show
       end
