25f0c0ba80030dcc7eea4a6ebbe89bec8f390355
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-25 22:34:29 -0600

    Update view and test for locate method

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -3,6 +3,7 @@ module Admin
     def show
       @application = Application.find(params[:id])
       if params[:status].present?
+        # should refactor below line into PetApp model with `locate(id, id)`
         pet_application = PetApplication.where(pet_id: params[:pet_id]).where(application_id: @application.id)[0]
         pet_application.update_status(params[:status])
       end
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -19,5 +19,16 @@ RSpec.describe PetApplication do
         expect(pet_application.status).to eq 'approved'
       end
     end
+
+    describe '#locate' do
+      it 'finds the pet application for a pet and application pair' do
+        frizz = Application.create!(name: 'Ms. Frizzle', street_address: '1 Magic Schoolbus Rd', city: 'Walkerville', state: 'MD', zip_code: '01010', description: 'Because I am a boss.', status: :in_progress)
+        shelter = Shelter.create!(name: 'All Star Pets', city: 'Walkerville', foster_program: true, rank: 4)
+        liz = frizz.pets.create!(name: 'Liz Ard', breed: "Jackson's chameleon", age: 7, adoptable: true, shelter: shelter)
+        pet_application = PetApplication.last
+
+        expect(PetApplication.locate(liz.id, frizz.id)).to eq pet_application
+      end
+    end
   end
 end
