ab0b910fd3385d94c77f4fad3a089decfb3cdf8b
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-27 14:29:24 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,12 +26,14 @@ shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_pr
 shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
 shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
 
-pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
 pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
 pet_3 = shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
 pet_4 = shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
 pet_5 = shelter_3.pets.create(name: 'Liz Ard', breed: "Jackson's chameleon", age: 7, adoptable: true)
-
+pet_6 = shelter_3.pets.create!(name: 'Catward', breed: 'bengal', age: 4, adoptable: true)
+pet_7 = shelter_2.pets.create!(name: 'Gus', breed: 'Black Lab', age: 4, adoptable: true)
+pet_8 = shelter_2.pets.create!(name: 'Doge', breed: 'Shiba-Inu', age: 7, adoptable: false)
 
 app_1 = pet_5.applications.create!(name: 'Ms. Frizzle', street_address: '1 Magic Schoolbus Rd', city: 'Walkerville', state: 'MD', zip_code: '01010', description: 'Because I am a boss.', status: :in_progress)
 app_2 = pet_2.applications.create!(name: 'Maude', street_address: '2 Sunflower Rd', city: 'Hillsborough', state: 'CA', zip_code: '02220', description: "My head is in the stars...", status: :pending)
