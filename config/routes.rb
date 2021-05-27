a829e4bfccf813e9489726a74f215fdd804c89ed
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-27 17:19:26 -0600

    Update vets and vet offices to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,11 @@
 Rails.application.routes.draw do
   root 'application#welcome'
 
-  resources :applications, :pet_applications, :pets, :shelters, :veterinarians
+  resources :applications, :pet_applications, :pets, :shelters, :veterinarians, :veterinary_offices
   namespace :admin do
     resources :shelters, only: [:index, :show]
     resources :applications, only: [:index, :show]
   end
-
-  get '/veterinary_offices', to: 'veterinary_offices#index'
-  get '/veterinary_offices/new', to: 'veterinary_offices#new'
-  get '/veterinary_offices/:id', to: 'veterinary_offices#show'
-  post '/veterinary_offices', to: 'veterinary_offices#create'
-  get '/veterinary_offices/:id/edit', to: 'veterinary_offices#edit'
-  patch '/veterinary_offices/:id', to: 'veterinary_offices#update'
-  delete '/veterinary_offices/:id', to: 'veterinary_offices#destroy'
-
-  get '/veterinarians', to: 'veterinarians#index'
-  get '/veterinarians/:id', to: 'veterinarians#show'
-  get '/veterinarians/:id/edit', to: 'veterinarians#edit'
-  patch '/veterinarians/:id', to: 'veterinarians#update'
-  delete '/veterinarians/:id', to: 'veterinarians#destroy'
 
   get '/shelters/:shelter_id/pets', to: 'shelters#pets'
   get '/shelters/:shelter_id/pets/new', to: 'pets#new'
